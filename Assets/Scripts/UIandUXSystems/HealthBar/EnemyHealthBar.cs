d665ecb7d16b9fa655cc631b7aa5c4f1f36f4937
BranBuddy <brandonwahl111@gmail.com>
2025-10-02 22:08:13 -0400

    added basic enemy health bar
    added a enemy healthbar prefab that reacts to changes with the enemy health.

--- Assets/Scripts/UIandUXSystems/HealthBar/EnemyHealthBar.cs
+++ Assets/Scripts/UIandUXSystems/HealthBar/EnemyHealthBar.cs
@@ -1,16 +1,34 @@
+using System.Net.NetworkInformation;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealthBar : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private BaseEnemy<EnemyState, EnemyTrigger> enemy;
+    private float health;
+    private float maxHealth;
+    [SerializeField] private Slider slider;
+
     void Start()
     {
+        enemy = this.GetComponent<BaseEnemy<EnemyState, EnemyTrigger>>();
+
+        slider.maxValue = enemy.maxHealth;
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        SetHealth();
+    }
+
+    internal void SetHealth()
+    {
+        health = enemy.currentHealth;
         
+        slider.value = health;
+
     }
 }
