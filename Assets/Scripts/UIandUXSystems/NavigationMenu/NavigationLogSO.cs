e0c2811ecee750a757527001d6051e4c63ab9472
BranBuddy <brandonwahl111@gmail.com>
2025-10-22 01:53:24 -0400

    Starting navigation log

---
+++ Assets/Scripts/UIandUXSystems/NavigationMenu/NavigationLogSO.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System;
+using UnityEditor;
+
+[ExecuteInEditMode]
+[CreateAssetMenu(fileName = "NavigationLogSO", menuName = "NavigationMenu/Logs", order = 1)]
+public class NavigationLogSO : ScriptableObject
+{
+    [field: SerializeField] public string logID { get; private set; }
+    public string logName;
+    public string locationFound;
+    public string dateFound;
+
+    [TextArea(3, 10)]
+    public string logDescription;
+
+    private void OnValidate()
+    {
+
+        #if UNITY_EDITOR
+        string idName = this.name.Replace("Log", "");
+        logID = "00" + idName;
+        EditorUtility.SetDirty(this);
+
+        #endif
+            
+        
+    }
+
+}
