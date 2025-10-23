c46ca192c8cf4885dae9042b1a12e1dd433167a8
BranBuddy <brandonwahl111@gmail.com>
2025-10-22 23:14:35 -0400

    reorganized logs and finetuned

---
+++ Assets/Scripts/UIandUXSystems/NavigationMenu/DiaryScripts/DiarySO.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "DiarySO", menuName = "NavigationMenu/Diaries", order = 1)]
+public class DiarySO : ScriptableObject
+{
+    [field: SerializeField] public string diaryID { get; private set; }
+
+    [TextArea(3, 10)]
+    public string diaryDescription;
+
+    //This ensures that the idName cannot be repeated
+    private void OnValidate()
+    {
+
+    #if UNITY_EDITOR
+        string idName = this.name.Replace("Diary", "");
+        diaryID = "#00" + idName;
+        EditorUtility.SetDirty(this);
+
+    #endif
+
+
+    }
+}
