a353c5e6f0d920e639b4dbe9bdb60faf73cb7e9f
BranBuddy <brandonwahl111@gmail.com>
2025-11-12 21:30:32 -0500

    added comments

--- Assets/Scripts/UIandUXSystems/MainMenu/SubMenuManager.cs
+++ Assets/Scripts/UIandUXSystems/MainMenu/SubMenuManager.cs
@@ -8,10 +8,15 @@ using UnityEngine;
 
 public class SubMenuManager : MonoBehaviour
 {
+    //DONT TOUCH THESE VARIABLES! They will auto populate when menus are enabled/disabled
     [SerializeField] internal GameObject lastActivatedSubMenu = null;
     [SerializeField] internal GameObject lastActivatedSettingsMenu = null;
     [SerializeField] internal GameObject currentSettingsMenu = null;
+
+    //Assign main menu container to this
     [SerializeField] internal GameObject mainMenu = null;
+
+    //Will change if the player is on the settings menu or not
     [SerializeField] internal bool isOnSettingsMenu = false;
 
     //When the settings button is clicked, this function is called
